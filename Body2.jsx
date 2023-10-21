f1eb8ab117572bbc0837d115a46866156522a11d
reddy982 <gharishreddy58@gmail.com>
2023-10-21 19:55:23 +0530

    4 of 2 commit

---
+++ Body2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const Body2 =() =>{
+    return(
+        <>
+        <hr/>
+        <h3>About Me</h3>
+        <p>Bhagat Singh was a charismatic Indian revolutionary who participated in the mistaken murder of a junior British police officer in what was to be retaliation for the death of an Indian nationalist. Wikipedia
+        Born: 28 September 1907, Banga, Pakistan
+        Died: 23 March 1931, Lahore Central Jail, Lahore, Pakistan
+        Parents: Sardar Kishan Singh Sandhu, Vidyavati
+        Siblings: Bibi Parkash Kaur, Kultar Singh, Bibi Amar Kaur, Kulbir Singh, Bibi Shakuntla, Rajinder Singh, Jagat Singh, Ranbir Singh
+        Height: 1.83 m
+        Organization founded: Naujawan Bharat Sabha[h]</p>
+        </>
+    )
+}
+export default Body2;
---
+++ Footer2.jsx
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+class Footer2 extends Component{
+    render(){
+        return(
+            <>
+            <hr/>
+            <h3>Contact</h3>
+            <p>
+                <b>Mobile:</b>7676596958
+            </p>
+            <p>
+                <b>Email:</b>sf@gmail.com
+            </p>
+
+            </>
+
+        );
+    }
+}
+export default Footer2;
