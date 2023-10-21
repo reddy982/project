65ca25c270fa9e7b8d0469fa094bf6114a8620ca
reddy982 <gharishreddy58@gmail.com>
2023-10-21 19:41:45 +0530

    3 of 2 commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Bootstrap from "./components/Bootstrap";
+const App = () =>{
+  return(
+    <>
+    <Bootstrap/>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
+  );
+};
+export default App;
+
+      
+
+      
+ 
