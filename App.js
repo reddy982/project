c4f7a026a2c0876259658eeca7ee8c2b0372a509
reddy982 <gharishreddy58@gmail.com>
2023-10-21 20:05:13 +0530

    4 of 2 commit

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import Header2 from "./components/Header2";
-import Body2 from "./components/Body2";
-import Footer2 from "./components/Footer2"
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-// import Bootstrap from "./components/Bootstrap";
+ import Bootstrap from "./components/Bootstrap";
 const App = () =>{
   return(
     <>
-    {/* <Bootstrap/> */}
+    <Bootstrap/>
     <Header/>
     <Body/>
     <Footer/>
-    <hr/>
-    <Header2/>
-    <Body2/>
-    <Footer2/>
     </>
   );
 };
