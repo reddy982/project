a85c080a1acc699fa1f26ec3a336e49ee0872488
reddy982 <gharishreddy58@gmail.com>
2023-10-21 20:06:52 +0530

    4 of  2 commit

---
+++ Bootstrap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+function Bootstrap(){
+    return(
+        <>
+        <h2 className="text-danger text-center">Bootstrp in react</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dicta modi optio iste inventore omnis deserunt quia dolorum sapiente aperiam eveniet, deleniti esse facilis adipisci est odio! Dolore, totam ipsa?</p>
+        </>
+
+    );
+}
+export default Bootstrap;
