2105de3a17f45745a6c8964b40bd9ecd39da6d60
reddy982 <gharishreddy58@gmail.com>
2023-10-21 19:36:41 +0530

    3 of 2 commit

---
+++ Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Body =() =>{
+    return(
+        <>
+        <hr/>
+        <h3>About Me</h3>
+        <p>Subhas Chandra Bose (/ʃʊbˈhɑːs ˈtʃʌndrə ˈboʊs/ ⓘ shuub-HAHSS CHUN-drə BOHSS;[12] 23 January 1897 – 18 August 1945) was an Indian nationalist whose defiance of British authority in India made him a hero among many Indians, but his wartime alliances with Nazi Germany and Imperial Japan left a legacy vexed by authoritarianism, anti-Semitism, and military failure. The honorific Netaji (Bengali: "Respected Leader") was first applied to Bose in Germany in early 1942—by the Indian soldiers of the Indische Legion and by the German and Indian officials in the Special Bureau for India in Berlin. It is now used throughout India.[h]</p>
+        </>
+    )
+}
+export default Body;
---
+++ Footer.jsx
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+class Footer extends Component{
+    render(){
+        return(
+            <>
+            <hr/>
+            <h3>Contact</h3>
+            <p>
+                <b>Mobile:</b>7676596958
+            </p>
+            <p>
+                <b>Email:</b>sf@gmail.com
+            </p>
+
+            </>
+
+        );
+    }
+}
+export default Footer;
