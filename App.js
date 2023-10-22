24a80ab93d839d8da6868de6fc8f1b8e90913167
reddy982 <gharishreddy58@gmail.com>
2023-10-22 13:47:19 +0530

    5 of 2 commit

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
- import Bootstrap from "./components/Bootstrap";
-const App = () =>{
+// import React from 'react';
+// import Header from "./components/Header";
+// import Body from "./components/Body";
+// import Footer from "./components/Footer";
+//  import Bootstrap from "./components/Bootstrap";
+// const App = () =>{
+//   return(
+//     <>
+//     <Bootstrap/>
+//     <Header/>
+//     <Body/>
+//     <Footer/>
+//     </>
+//   );
+// };
+// export default App;
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Login2 from "./pages/Login2";
+// import Signup from "./Pages/Signup";
+
+import{BrowserRouter,Routes,Route} from "react-router-dom";
+import Signup from "./Pages/Signup";
+
+function App(){
   return(
     <>
-    <Bootstrap/>
-    <Header/>
-    <Body/>
-    <Footer/>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route element={<Home/>} path="/" />
+          <Route element={<Contact/>} path="/contact" />
+          <Route element={<About/>} path="/about" />
+          <Route element={<Login2/>} path="/login2" />
+          {/* <Route element={<Signup/>} path="/signup" /> */}
+        </Routes> 
+          </BrowserRouter>
     </>
-  );
-};
+  )
+}
 export default App;
