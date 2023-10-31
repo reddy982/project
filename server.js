838a285f77fd9e3dc7f9133cdae4cf01ddc7cb2f
reddy982 <gharishreddy58@gmail.com>
2023-10-31 09:03:53 +0530

    2 0f 3 commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 console.log("Hello Every one")
 const express=require("express")
+const mongoose=require("mongoose")
+const routes=require("./routes/route");
 const app=express()
+app.use(express.json());
+app.use("/",routes);
+//Db connection
+mongoose.connect(
+    "mongodb+srv://Harish:harish%402003@cluster0.10putry.mongodb.net/"
+).then(()=>{
+    console.log("Database is connected successfully")
+})
+.catch((err)=>{
+    console.log(err,"something went wrong");
+});
 
 //Test API
 app.get("/test",(req,res)=>{   //routing part
