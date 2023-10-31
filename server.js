39d7d76fb770670c35a6c56de2a91c7433536c3f
reddy982 <gharishreddy58@gmail.com>
2023-10-31 08:46:19 +0530

    1 of 3 commit

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-//console.log("Hello Every one")
+console.log("Hello Every one")
 const express=require("express")
-const mongoose=require("mongoose")
 const app=express()
-//Db connection
-mongoose.connect(
-    "mongodb+srv://Harish:harish%402003@cluster0.10putry.mongodb.net/"
-).then(()=>{
-    console.log("Database is connected successfully")
-})
-.catch((err)=>{
-    console.log(err,"something went wrong");
-});
-
 
 //Test API
-app.get("/test",(req,res)=>{
+app.get("/test",(req,res)=>{   //routing part
     res.send("Hello Everyone,This is test API")
 });
-app.listen(6000,()=>{
+app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 });
